62b44d1d310fedde67b4d4cee920d85f1a7a8b42
Wagner Loch <wagner_loch@hotmail.com>
2026-08-18 10:38:37 -0300

    Add greetings function

--- Main.js
+++ Main.js
@@ -1,3 +1,9 @@
 console.log("Hello, World!");
 
 console.log("Bem-vindos a terceira aula de Engenharia de Software II!");
+
+function greet(name) {
+    console.log(`Olá, ${name}!`);
+}
+
+greet("Wagner");
