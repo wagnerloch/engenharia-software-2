02822c30904528f6857eb9add7d34827abaed7a0
Wagner Loch <wagner_loch@hotmail.com>
2026-08-18 10:51:29 -0300

    Fix greetings function

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ console.log("Hello, World!");
 console.log("Bem-vindos a terceira aula de Engenharia de Software II!");
 
 function greet(name) {
-    console.log(`Olá, ${name}!`);
+    console.log(`Oi, tudo bem ${name}?`);
 }
 
 greet("Wagner");
